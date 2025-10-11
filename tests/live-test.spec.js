3dfa0dfb38ce2044d8d9d0040ef39a838a00cd9f
Sergio Hernandez <sergiohernandezlara07@gmail.com>
2025-10-11 11:29:45 +0200

    chore: remove old configuration files and restructure project

--- tests/live-test.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Experimento en vivo', async ({ page }) => {
-  // Aquí pegaremos el código que genere el inspector
-  await page.goto('http://localhost:5173/login');
-  
-  // TODO: Pegar código del inspector aquí
-  
-});
